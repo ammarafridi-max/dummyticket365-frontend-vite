a4f96608604b5017c322be07029f061bde7fe805
Ammar Afridi <ammar.afridi95@gmail.com>
2026-01-08 09:40:39 +0400

    Move queryClient into App + import AppRoutes

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,19 @@
 import '@fontsource-variable/nunito';
 import '@fontsource/merriweather';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import { ToastContainer, Zoom } from 'react-toastify';
 import { useEffect } from 'react';
-import { initializeGA } from './utils/analytics';
-import Layout from './components/Layout';
-import BookingLayout from './components/BookingLayout';
-import Home from './pages/Home';
-import PageNotFound from './pages/PageNotFound';
-import FAQ from './pages/FAQ';
-import SelectFlights from './pages/SelectFlights';
-import ReviewDetails from './pages/ReviewDetails';
-import PaymentSuccess from './pages/PaymentSuccess';
-import TermsAndConditions from './pages/TermsAndConditions';
-import PrivacyPolicy from './pages/PrivacyPolicy';
-import FlightReservation from './pages/FlightReservation';
-import ScrollToTop from './components/ScrollToTop';
-import DummyTicket from './pages/DummyTicket';
-import Sitemap from './pages/Sitemap';
+import { initializeGA } from './lib/analytics';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import AppRoutes from './app/AppRoutes';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 300 * 1000,
+    },
+  },
+});
 
 function App() {
   useEffect(() => {
@@ -28,29 +23,9 @@ function App() {
   return (
     <HelmetProvider>
       <ToastContainer transition={Zoom} />
-      <BrowserRouter>
-        <ScrollToTop />
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="*" element={<PageNotFound />} />
-            <Route path="flight-reservation" element={<FlightReservation />} />
-            <Route path="dummy-ticket" element={<DummyTicket />} />
-            <Route path="sitemap" element={<Sitemap />} />
-            <Route path="faq" element={<FAQ />} />
-            <Route path="payment-successful" element={<PaymentSuccess />} />
-            <Route
-              path="terms-and-conditions"
-              element={<TermsAndConditions />}
-            />
-            <Route path="privacy-policy" element={<PrivacyPolicy />} />
-            <Route path="booking" element={<BookingLayout />}>
-              <Route path="select-flights" element={<SelectFlights />} />
-              <Route path="review-details" element={<ReviewDetails />} />
-            </Route>
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <AppRoutes />
+      </QueryClientProvider>
     </HelmetProvider>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -4,19 +4,11 @@ import { Provider } from 'react-redux';
 import './index.css';
 import App from './App.jsx';
 import store from './redux/store';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { HelmetProvider } from 'react-helmet-async';
-import { ToastContainer, Zoom } from 'react-toastify';
-import { BrowserRouter, Routes } from 'react-router-dom';
-
-const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
-  <QueryClientProvider client={queryClient}>
-    <StrictMode>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </StrictMode>
-  </QueryClientProvider>
+  <StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>
 );
