eb825f6e7a2e185b9faa72550ea1b97c329ea6fa
Ammar Afridi <ammar.afridi95@gmail.com>
2026-01-08 09:38:47 +0400

    Create app folder for AppRoutes

---
+++ src/app/AppRoutes.jsx
@@ -0,0 +1,50 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ScrollToTop from '../components/ScrollToTop';
+
+// Layout
+import AppLayout from '../layouts/AppLayout';
+import BookingLayout from '../layouts/BookingLayout';
+
+// Landing Pages
+import Home from '../pages/landing-pages/Home';
+import FlightReservation from '../pages/landing-pages/FlightReservation';
+
+// Booking Pages
+import SelectFlights from '../pages/booking-pages/SelectFlights';
+import ReviewDetails from '../pages/booking-pages/ReviewDetails';
+import PaymentSuccess from '../pages/booking-pages/PaymentSuccess';
+
+// Legal Pages
+import TermsAndConditions from '../pages/legal-pages/TermsAndConditions';
+import PrivacyPolicy from '../pages/legal-pages/PrivacyPolicy';
+
+// FAQ
+import FAQ from '../pages/faq/FAQ';
+
+// Other Pages
+import Sitemap from '../pages/other/Sitemap';
+import PageNotFound from '../pages/other/PageNotFound';
+
+export default function AppRoutes() {
+  return (
+    <BrowserRouter>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<AppLayout />}>
+          <Route index element={<Home />} />
+          <Route path="flight-reservation" element={<FlightReservation />} />
+          <Route path="*" element={<PageNotFound />} />
+          <Route path="sitemap" element={<Sitemap />} />
+          <Route path="faq" element={<FAQ />} />
+          <Route path="payment-successful" element={<PaymentSuccess />} />
+          <Route path="terms-and-conditions" element={<TermsAndConditions />} />
+          <Route path="privacy-policy" element={<PrivacyPolicy />} />
+          <Route path="booking" element={<BookingLayout />}>
+            <Route path="select-flights" element={<SelectFlights />} />
+            <Route path="review-details" element={<ReviewDetails />} />
+          </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
