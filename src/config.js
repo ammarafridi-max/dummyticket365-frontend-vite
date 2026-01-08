381484a9847eaa7d6c13ab015c6bdf0540606215
Ammar Afridi <ammar.afridi95@gmail.com>
2026-01-08 09:41:07 +0400

    Create new variable GA4_MEASUREMENT_ID + rename BASEURL to BACKEND

--- src/config.js
+++ src/config.js
@@ -1,2 +1,3 @@
-export const BASEURL = import.meta.env.VITE_BACKEND_URL;
+export const BACKEND = import.meta.env.VITE_BACKEND_URL;
 export const VIEWTRIP_URL = import.meta.env.VITE_VIEWTRIP_BACKEND;
+export const GA4_MEASUREMENT_ID = import.meta.env.VITE_GA4_MEASUREMENT_ID;
